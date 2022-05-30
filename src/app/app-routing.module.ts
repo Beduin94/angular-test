262c962c22d238e09fe0f31bc597c7d9583aaa6a
beduin <beduin94@mail.ru>
2022-05-31 00:59:13 +1000

    modules, routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'data',
+        loadChildren: () => import('./modules/data/data.module').then((m) => m.DataModule)
+      },
+      {
+        path: 'projects',
+        loadChildren: () => import('./modules/projects/projects.module').then((m) => m.ProjectsModule)
+      },
+      {
+        path: '',
+        redirectTo: '/data',
+        pathMatch: 'full',
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
