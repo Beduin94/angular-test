1b2a3ef8bd99ad6addfd249bd61f45eb6fee8144
beduin <beduin94@mail.ru>
2022-05-31 01:41:48 +1000

    nav-bar, scss

--- src/app/app-common/components/nav-bar/nav-bar.component.ts
+++ src/app/app-common/components/nav-bar/nav-bar.component.ts
@@ -3,9 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
+
+  navLinks = [
+    {
+      label: 'Данные',
+      link: 'data',
+    },
+    {
+      label: 'Проекты',
+      link: 'projects',
+    },
+  ];
 
   constructor() { }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
+import {AppCommonModule} from "./app-common/app-common.module";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import {AppRoutingModule} from "./app-routing.module";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppCommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
