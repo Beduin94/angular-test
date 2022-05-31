afe50e1ab110efda93058d617a128b6a241eba10
beduin <beduin94@mail.ru>
2022-06-01 03:47:19 +1000

    until destroyed

--- src/app/modules/projects/project-list/project-list.component.ts
+++ src/app/modules/projects/project-list/project-list.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IProject } from "../../../app-common/interface/project";
 import { ProjectService } from "../../../app-common/services/project/project.service";
 import { Observable } from "rxjs";
+import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
 
+@UntilDestroy()
 @Component({
   selector: 'app-project-list',
   templateUrl: './project-list.component.html',
   styleUrls: ['./project-list.component.scss']
 })
-export class ProjectListComponent implements OnInit {
+export class ProjectListComponent implements OnInit, OnDestroy {
 
   projects: IProject[];
   projects$: Observable<IProject[]> = this.projectService.projects.asObservable();
@@ -16,7 +18,7 @@ export class ProjectListComponent implements OnInit {
   constructor(
     private projectService: ProjectService
   ) {
-    this.projects$.pipe().subscribe((projects) => {
+    this.projects$.pipe(untilDestroyed(this)).subscribe((projects) => {
       if (projects) this.projects = projects;
     });
   }
@@ -24,4 +26,6 @@ export class ProjectListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  ngOnDestroy(): void {
+  }
 }
